ee31e1e4672fb8e0d0d97f6209148eb8d3bd464f
JitosDev <bos.smpn1kts@gmail.com>
2024-10-31 11:40:58 +0700

    Git initialization

---
+++ resources/views/keranjang.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('title', 'Beranda Laravel')
+@section('content')
+    <h1 class="text-center mt-5">Selamat Datang di Beranda
+        Laravel</h1>
+    <p class="text-center">Ini adalah tampilan sederhana menggunakan
+        Blade Template di Laravel.</p>
+@endsection
