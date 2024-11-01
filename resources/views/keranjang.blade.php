8b2a5fc7d4648a4a474551daf6b055abe466bee4
JitosDev <bos.smpn1kts@gmail.com>
2024-11-01 17:22:55 +0700

    Mengedit keranjang

--- resources/views/keranjang.blade.php
+++ resources/views/keranjang.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('title', 'Keranjang')
 @section('content')
-    <h1 class="text-center mt-5">Ini adalah halaman coba Keranjang</h1>
+    <h1 class="text-center mt-5">Ini adalah halaman coba Katalog</h1>
     <p class="text-center">Ini adalah tampilan sederhana menggunakan Blade Template di Laravel.</p>
 @endsection
